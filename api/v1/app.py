301e40b7f4ae6e7cf8104cbceb9b5397a538dba4
Monarch <monarchofaether@gmail.com>
2024-08-04 19:55:04 +0300

    everything up to date

---
+++ api/v1/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" API for HBNB Clone
+"""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from sys import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown.appcontext
+def close():
+    storage.close()
+
+if __name__ == '__main__':
+    if getenv('HBNB_API_HOST') is not None:
+        host = getenv('HBNB_API_HOST')
+    else:
+        host = '0.0.0.0'
+    if getenv('HBNB_API_PORT') is not None:
+        port = getenv('HBNB_API_PORT')
+    else:
+        port = 5000
+    app.run(host=host, port=port)
---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Blueprint Views
+"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+@app_views.route('/status'):
+def status():
+    return jsonify({'status': 'OK'})
+
+@app_views.route('/stats'):
+def stats():
+    return jsonify(storage.count())
