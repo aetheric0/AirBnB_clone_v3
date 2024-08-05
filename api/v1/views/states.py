308e6f7db993ffbd41aef52892b6f68653ce0693
Monarch <monarchofaether@gmail.com>
2024-08-06 00:21:48 +0300

    pycodestyle verified, documentation added

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,8 +6,11 @@ from api.v1.views import app_views
 
 state_objects = storage.all(State)
 
+
 @app_views.route('/states', strict_slashes=False)
 def get_states():
+    """ Retrieves states from storage
+    """
     retrieved_states = []
     for state in state_objects.values():
         retrieved_states.append(state.to_dict())
@@ -16,6 +19,8 @@ def get_states():
 
 @app_views.route('/states/<state_id>', strict_slashes=False)
 def get_state_by_id(state_id):
+    """ Retrieves one state from sotrage based on the id
+    """
     state_id = str(state_id)
     try:
         return jsonify(storage.get(State, state_id).to_dict()), 200
@@ -23,8 +28,11 @@ def get_state_by_id(state_id):
         abort(404)
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state_by_id(state_id):
+    """ Deletes the state from storage by its given id
+    """
     state = storage.get(State, state_id)
     if state:
         storage.delete(state)
@@ -36,6 +44,8 @@ def delete_state_by_id(state_id):
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
+    """ Adds a new state to storage
+    """
     body = request.get_json()
     if body is None:
         abort(400, 'Not a JSON')
@@ -45,8 +55,11 @@ def post_state():
     new_state.save()
     return jsonify(new_state.to_dict()), 201
 
+
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
+    """ Modifies the attributes of a state if they are not dates or id
+    """
     state = storage.get(State, state_id)
     if state:
         body = request.get_json()
