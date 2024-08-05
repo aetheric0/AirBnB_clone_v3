e628a0451d0ae01992684c22c2c7d1271da90703
Monarch <monarchofaether@gmail.com>
2024-08-05 15:46:23 +0300

    task 3, implement api end of backend

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,10 +5,10 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
-@app_views.route('/status'):
+@app_views.route('/status')
 def status():
     return jsonify({'status': 'OK'})
 
-@app_views.route('/stats'):
+@app_views.route('/stats')
 def stats():
     return jsonify(storage.count())
